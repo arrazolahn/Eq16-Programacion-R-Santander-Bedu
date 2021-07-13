41ab60701b6f45d6a635861273510e6afdc53da1
arrazolahn <35963381+arrazolahn@users.noreply.github.com>
2021-07-13 10:09:26 +0200

    Add files via upload

---
+++ Postwork07/Postwork7.R
@@ -0,0 +1,19 @@
+#Utilizando el manejador de BDD Mongodb Compass (previamente instalado), deberás de realizar las siguientes acciones:
+library("mongolite")
+
+#Alojar el fichero match.data.csv en una base de datos llamada match_games, nombrando al collection como match
+mongoDB <- mongo("match", url = "mongodb+srv://arrazolan:Pippin300151@cluster0.axpl7.mongodb.net/match_games?retryWrites=true&w=majority")
+
+#Una vez hecho esto, realizar un count para conocer el número de registros que se tiene en la base
+mongoDB$count()
+
+"Realiza una consulta utilizando la sintaxis de Mongodb en la base de datos, 
+para conocer el número de goles que metió el Real Madrid el 20 de diciembre de 2015 y 
+contra que equipo jugó, ¿perdió ó fue goleada?"
+
+golesRM201215 <- mongoDB$find(
+  query = '{"Date":"2015-12-20","HomeTeam":"Real Madrid"}'
+)
+  
+#Por último, no olvides cerrar la conexión con la BDD
+rm(mongoDB)
